02f0ebaa28d90277a43b8ad6b97080a70d0b9053
Sean Gilligan <sean@msgilligan.com>
2015-02-01 12:14:50 -0800

    Add some missing comments

--- groovylang-support/groovy-demo/src/main/groovy/com/msgilligan/moneta/MonetaryAmountCategory.groovy
+++ groovylang-support/groovy-demo/src/main/groovy/com/msgilligan/moneta/MonetaryAmountCategory.groovy
@@ -3,10 +3,17 @@ package com.msgilligan.moneta
 import javax.money.MonetaryAmount
 
 /**
- *
+ * Category to add methods to MonetaryAmount to support operator overloading.
  */
 @Category(MonetaryAmount)
 class MonetaryAmountCategory {
+
+    /**
+     * MonetaryAmount has add() not plus(), let's remedy that
+     *
+     * @param right right operator
+     * @return sum as a MonetaryAmount
+     */
     MonetaryAmount plus(MonetaryAmount right) {
         return this.add(right)
     }
