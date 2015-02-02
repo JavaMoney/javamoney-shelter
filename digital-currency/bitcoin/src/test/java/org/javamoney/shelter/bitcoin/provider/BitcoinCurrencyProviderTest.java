7f6f5a2fb33b4252d4f8594e93e9d45e417dcbd2
Sean Gilligan <sean@msgilligan.com>
2015-02-01 21:16:49 -0800

    First cut at BitcoinCurrencyProvider
    I started with the sample code from the Moneta User’s Guide
    (which I think is out of date) I’m still learning the SPI so I may
    be doing something seriously wrong.
    
    I wrote 5 tests that are passing. Feel free to change the tests
    or add some to show what is missing.

---
+++ digital-currency/bitcoin/src/test/java/org/javamoney/shelter/bitcoin/provider/BitcoinCurrencyProviderTest.java
@@ -0,0 +1,65 @@
+package org.javamoney.shelter.bitcoin.provider;
+
+import org.junit.Test;
+
+import javax.money.CurrencyQuery;
+import javax.money.CurrencyQueryBuilder;
+import javax.money.CurrencyUnit;
+import javax.money.MonetaryCurrencies;
+import javax.money.UnknownCurrencyException;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * First cut at a test for a BitcoinCurrencyProvider
+ *
+ * @author Sean Gilligan
+ */
+public class BitcoinCurrencyProviderTest {
+
+    @Test
+    public void canCreateNewInstance() {
+        BitcoinCurrencyProvider provider = new BitcoinCurrencyProvider();
+        assertNotNull(provider);
+    }
+
+    @Test
+    public void returnsBitcoinForEmptyQuery() {
+        BitcoinCurrencyProvider provider = new BitcoinCurrencyProvider();
+        assertNotNull(provider);
+        CurrencyQuery query = CurrencyQueryBuilder.of().build();
+        Set<CurrencyUnit> currencies = provider.getCurrencies(query);
+        assertEquals("returned 1 currency", 1, currencies.size());
+        CurrencyUnit btc = (CurrencyUnit) currencies.toArray()[0];
+        assertEquals("currency is BTC", "BTC", btc.getCurrencyCode());
+    }
+
+    @Test
+    public void returnsBitcoinForBTCQuery() {
+        BitcoinCurrencyProvider provider = new BitcoinCurrencyProvider();
+        assertNotNull(provider);
+        CurrencyQuery query = CurrencyQueryBuilder.of().setCurrencyCodes("BTC").build();
+        Set<CurrencyUnit> currencies = provider.getCurrencies(query);
+        assertEquals("returned 1 currency", 1, currencies.size());
+        CurrencyUnit btc = (CurrencyUnit) currencies.toArray()[0];
+        assertEquals("currency is BTC", "BTC", btc.getCurrencyCode());
+    }
+
+    @Test
+    public void returnsEmptyForUSDQuery() {
+        BitcoinCurrencyProvider provider = new BitcoinCurrencyProvider();
+        assertNotNull(provider);
+        CurrencyQuery query = CurrencyQueryBuilder.of().setCurrencyCodes("USD").build();
+        Set<CurrencyUnit> currencies = provider.getCurrencies(query);
+        assertEquals("returned 0 currencies", 0, currencies.size());
+    }
+
+    @Test(expected=UnknownCurrencyException.class)
+    public void canNOTBeFoundViaMonetaryCurrencies() {
+        // Since we're not registered properly MonetaryCurrencies.getCurrency should throw an exception
+        CurrencyUnit btc = MonetaryCurrencies.getCurrency("BTC");
+    }
+
+}
