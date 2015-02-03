de473332c8d27cdc80c1434ff883505b5e6bbf83
Sean Gilligan <sean@msgilligan.com>
2015-02-03 11:46:00 -0800

    Add two more tests

--- digital-currency/bitcoin/src/test/java/org/javamoney/shelter/bitcoin/provider/BitcoinCurrencyProviderTest.java
+++ digital-currency/bitcoin/src/test/java/org/javamoney/shelter/bitcoin/provider/BitcoinCurrencyProviderTest.java
@@ -1,10 +1,13 @@
 package org.javamoney.shelter.bitcoin.provider;
 
+import org.javamoney.moneta.CurrencyUnitBuilder;
+import org.javamoney.moneta.Money;
 import org.junit.Test;
 
 import javax.money.CurrencyQuery;
 import javax.money.CurrencyQueryBuilder;
 import javax.money.CurrencyUnit;
+import javax.money.MonetaryAmount;
 import javax.money.MonetaryCurrencies;
 import javax.money.UnknownCurrencyException;
 import java.util.Set;
@@ -60,6 +63,32 @@ public class BitcoinCurrencyProviderTest {
     public void canNOTBeFoundViaMonetaryCurrencies() {
         // Since we're not registered properly MonetaryCurrencies.getCurrency should throw an exception
         CurrencyUnit btc = MonetaryCurrencies.getCurrency("BTC");
+    }
+
+    @Test()
+    public void WeCanMakeMoneyNow() {
+        // Since we're not registered properly MonetaryCurrencies.getCurrency should throw an exception
+        BitcoinCurrencyProvider provider = new BitcoinCurrencyProvider();
+        assertNotNull(provider);
+        CurrencyQuery query = CurrencyQueryBuilder.of().setCurrencyCodes("BTC").build();
+        Set<CurrencyUnit> currencies = provider.getCurrencies(query);
+        assertEquals("returned 1 currency", 1, currencies.size());
+        CurrencyUnit btc = (CurrencyUnit) currencies.toArray()[0];
+        assertEquals("currency is BTC", "BTC", btc.getCurrencyCode());
+
+        MonetaryAmount oneBitcoin = Money.of(1, btc);
+        assertNotNull(oneBitcoin);
+        assertEquals("number is correct", 1L, oneBitcoin.getNumber().longValueExact());
+        assertEquals("units is correct", btc, oneBitcoin.getCurrency());
+    }
+
+    @Test
+    public void WeDoNotReallyNeedAProviderToMakeMoney() {
+        CurrencyUnit btc = CurrencyUnitBuilder.of("BTC", "ad hoc btc provider").build();
+        MonetaryAmount oneBitcoin = Money.of(1, btc);
+        assertNotNull(oneBitcoin);
+        assertEquals("number is correct", 1L, oneBitcoin.getNumber().longValueExact());
+        assertEquals("units is correct", btc, oneBitcoin.getCurrency());
     }
 
 }
