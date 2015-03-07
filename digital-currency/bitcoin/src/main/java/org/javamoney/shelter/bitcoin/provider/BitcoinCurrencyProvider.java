8337aa892ac5891deb61fef882f26317980b7945
Werner Keil <werner.keil@gmx.net>
2015-03-07 20:05:36 +0100

    4: Implement Money and Currency SPI for Bitcoin
    Task-Url: http://github.com/JavaMoney/javamoney-shelter/issues/issue/4

--- digital-currency/bitcoin/src/main/java/org/javamoney/shelter/bitcoin/provider/BitcoinCurrencyProvider.java
+++ digital-currency/bitcoin/src/main/java/org/javamoney/shelter/bitcoin/provider/BitcoinCurrencyProvider.java
@@ -1,26 +1,36 @@
+/*
+ * Copyright (c) 2013, 205, Werner Keil and others by the @author tag.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.javamoney.shelter.bitcoin.provider;
 
 import javax.money.CurrencyContext;
 import javax.money.CurrencyContextBuilder;
 import javax.money.CurrencyQuery;
 import javax.money.CurrencyUnit;
-import javax.money.MonetaryAmountFactoryQuery;
-import javax.money.MonetaryAmountFactoryQueryBuilder;
-import javax.money.MonetaryContext;
 import javax.money.spi.CurrencyProviderSpi;
 import java.util.Collections;
-import java.util.Currency;
 import java.util.HashSet;
 import java.util.Set;
 
 import org.javamoney.moneta.CurrencyUnitBuilder;
-import org.javamoney.moneta.Money;
 
 /**
  *  A clueless attempt at a BitcoinCurrencyProvider based on (out of date?) code
  *  in the Moneta User's Guide.
  *
  * @author Sean Gilligan
+ * @author Werner Keil
  */
 public final class BitcoinCurrencyProvider implements CurrencyProviderSpi {
 
@@ -60,5 +70,10 @@ public final class BitcoinCurrencyProvider implements CurrencyProviderSpi {
         }
         return Collections.emptySet();
     }
+
+	@Override
+	public boolean isCurrencyAvailable(CurrencyQuery query) {
+		return !getCurrencies(query).isEmpty();
+	}
 
 }
