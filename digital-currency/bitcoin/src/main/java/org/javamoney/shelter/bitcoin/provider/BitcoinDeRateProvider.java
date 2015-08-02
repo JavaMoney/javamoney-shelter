1b6e46aa4d4ace7cd51305fd2c90ad31fe885bb7
Werner Keil <werner.keil@gmx.net>
2015-08-02 17:35:34 +0200

    18: Build bitcoin module (all modules) with Gradle
    Task-Url: http://github.com/JavaMoney/javamoney-shelter/issues/issue/18

--- digital-currency/bitcoin/src/main/java/org/javamoney/shelter/bitcoin/provider/BitcoinDeRateProvider.java
+++ digital-currency/bitcoin/src/main/java/org/javamoney/shelter/bitcoin/provider/BitcoinDeRateProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, 2014, Werner Keil and others by the @author tag.
+ * Copyright (c) 2013, 2015, Werner Keil and others by the @author tag.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -37,7 +37,7 @@ import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.map.ObjectMapper;
 // TODO consider switching to JSR 353
 
-import org.javamoney.moneta.convert.ExchangeRateBuilder;
+import org.javamoney.moneta.ExchangeRateBuilder;
 import org.javamoney.moneta.spi.DefaultNumberValue;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
